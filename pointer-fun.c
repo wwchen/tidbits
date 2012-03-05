0727d20de09877f9d365d22611114284fc9d6548
William Chen <williamchen16@gmail.com>
2012-03-04 22:17:14 -0600

    Initial commit

---
+++ pointer-fun.c
@@ -0,0 +1,15 @@
+char *c[]={
+  "ENTER",
+  "NEW",
+  "POINT",
+  "FIRST"
+};
+char **cp[]={c+3,c+2,c+1,c};
+char ***cpp=cp;
+main()
+{
+  printf("%s",**++cpp);
+  printf("%s ",*--*++cpp+3);
+  printf("%s",*cpp[-2]+3);
+  printf("%s\n",cpp[-1][-1]+1);
+}
