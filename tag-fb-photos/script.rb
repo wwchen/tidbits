34f18f0b215f2774980658ef3fc2331e93953349
William Chen <williamchen16@gmail.com>
2013-04-02 23:36:36 -0700

    Progress! Almost complete, but error prone
    v create an album
    v upload jpgs
    v tag pictures based on xmp tags and matching userid yml file
    - app page to request oauth token
    - check validity of album id and such
    - no break or ask for permissions
    - require user signoff before posting them all, though...
      "publish_stream is optional - if not present, the tag will go through
      Tag Review of the tagged person if s/he has this privacy setting
      enabled"

--- tag-fb-photos/script.rb
+++ tag-fb-photos/script.rb
@@ -3,19 +3,71 @@
 require 'rubygems'
 require 'koala'
 require 'exifr'
-require 'yaml'
+require 'xmp'
+require 'yamler'
 
-@config = YAML.load_file(File.join(File.dirname(File.expand_path(__FILE__)),"config.yml")) unless defined? CONFIG
-graph = Koala::Facebook::API.new(@config[oauth_token])
+def load_yaml(path)
+  return Yamler.load(File.join(File.dirname(File.expand_path(__FILE__)), path))
+end
+
+config  = load_yaml 'config.yml'
+userids = load_yaml 'userid_mapping.yml'
+graph   = Koala::Facebook::API.new(config['oauth_token'])
+
+# create an album if one isn't specified
+album_id = config['album_id']
+if album_id.nil? do
+  STDOUT.write "Creating an album... "
+  resp = graph.put_object('me', 'albums', {:name => 'Japan'})
+  album_id = resp['id']
+  STDOUT.puts (album_id.nil? ? "failed!" : album_id)
+else
+  puts "Using album id: #{album_id}"
+end
+
+# Find all jpgs in the folder
+pics = Dir[config['photos_dir'].sub(/\/$/, '') + '/*.jpg']
+pics.each do |pic|
+  puts "Processing #{pic}"
+  exif =  XMP.parse(EXIFR::JPEG.new(pic)).dc
+  tags =  exif.subject
+  title = exif.respond_to? 'title' ? exif.title : ''
+  caption = exif.respond_to? 'caption' ? exif.caption : ''
+  message = [title, caption].join("\n").strip
+
+  puts "=> Found tags: #{tags.join ', '}"
+  id_tags = userids.select {|k,v| tags.include? k}
+  puts "=> Matching user ids: #{id_tags.keys}"
+
+  # upload the picture
+  STDOUT.write "Uploading picture... "
+  
+  resp = graph.put_picture(pic, {:message => message}, album_id)
+  photo_id = resp['id']
+  STDOUT.puts photo_id.nil? ? "failed!" : photo_id
+  
+  # tag the picture
+  # https://developers.facebook.com/docs/reference/api/photo/, tags#create
+  STDOUT.write "Tagging picture... "
+  tag_params = Hash[id_tags.map {|k,v| ['tag_uid', v]}]
+  resp = graph.put_object(photo_id, 'tags', tag_params)
+  STDOUT.puts resp ? "Success!" : "Failed!"
+
+  puts "\n"
+end
+exit
+
+class Object
+  def or_if(method, val = nil)
+    self.send(method) ? (block_given? ? yield : val) : self
+  end         
+end
+
 
 # reading tags
 # https://github.com/arsduo/koala/wiki
 # https://developers.facebook.com/docs/reference/api/photo/
-resp = graph.put_object('me/albums', {:name => 'Japan'})
-album_id = resp.id
-resp = graph.put_picture(filename, {:title => 'blah'})
-photo_id = resp.id
-graph.put_object(photo_id, {:to => user_id})
+
 
 # http://rdoc.info/github/remvee/exifr
 # http://rubydoc.info/gems/xmp/0.2.0/frames
